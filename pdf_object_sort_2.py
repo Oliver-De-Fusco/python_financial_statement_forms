d8a21217220929cecea02c574a807d0bb7b257f7
Oliver <oliverd@live.co.uk>
2023-06-01 22:11:55 +0100

    handles duplicates

--- pdf_object_sort_2.py
+++ pdf_object_sort_2.py
@@ -1,18 +1,115 @@
-from pdf_reader import document
 import os
+from itertools import cycle
 
-reports = [document(filename) for filename in os.listdir(".") if os.path.isfile(filename) if filename[-3:] == "pdf"]
+from dateutil.relativedelta import *
 
-reports.sort(key=lambda document:document.date_ended)
+from pdf_reader import document
 
-def q_generator():
-    while True:
-        for i in range(4):
-            yield i + 1
 
-for report, quater in zip(reports, q_generator()):
-    if report.name == f"{report.date_ended.year}Q{quater} - {report.date_ended}.pdf":
-        continue
-    else:
-        print(f"{report} --> {report.date_ended.year}Q{quater} - {report.date_ended}.pdf")
-        report.set_name(f"{report.date_ended.year}Q{quater} - {report.date_ended}.pdf")
+def expected_seqeunce(start, end):
+
+    expected = [start]
+
+    while expected[-1] < end:
+
+        # you have to specify "months" not "month" otherwise the initial month will become January
+        next_month = expected[-1] + relativedelta(months=3)
+        expected.append(next_month)
+
+    return expected
+
+
+def truncate_day(input):
+    return str(input)[:-3]
+
+
+def truncate_day_generator(input_list):
+
+    for x in input_list:
+        yield truncate_day(x)
+
+
+def remove_duplicates(input_list):
+
+    output = [input_list[0]]
+    duplicates = []
+
+    for report in input_list:
+        if truncate_day(report.date_ended) == truncate_day(output[-1].date_ended):
+            duplicates.append(report)
+        else:
+            output.append(report)
+
+    return output, duplicates[1:]
+
+
+def fill_missing_gaps(input_list, expected_list, fill_value=None):
+
+    # we only care about comparing years and months so remove days
+    input_dates = list(truncate_day(x.date_ended) for x in report_list)
+
+    # By comparing the expected vs actual 
+    # we know when to insert a new value into the original list by looping over it
+    for index, expected_date in enumerate(expected_list):
+        if not expected_date in input_dates:
+            input_list.insert(index, fill_value)
+
+    return input_list
+
+
+def find_first_10K(input_list):
+
+    for index, report in enumerate(input_list):
+        if report:
+            if report.report_type == "10-K":
+                return index
+
+
+def sort_files(input_files, generator):
+    first_k_10 = find_first_10K(input_files)
+
+    # prep the generator for the sequence
+    # if you altered the length of the generator you need to change this as well
+    for _ in range(4 - first_k_10):
+        next(generator)
+    for report in input_files:
+        # Thanks to the fill_missing_gaps() generating the correct sequence is incredibly easy
+        if report:
+            report.set_name(
+                f"{report.company} - {report.date_ended.year} {next(generator)}")
+        else:
+            next(generator)
+
+
+# Code starts here
+if __name__ == "__main__":
+
+    # get the pdf files in current directory
+    report_list = [document(filename) for filename in os.listdir(".")
+                   if os.path.isfile(filename) if filename[-4:] == ".pdf"]
+
+    # remove files from list that data was not able to be found for
+    report_list = [report for report in report_list if (
+        report.company or report.date_ended or report.report_type)]
+
+    # The list has to be sorted by date and have no duplicates
+    report_list.sort(key=lambda document: document.date_ended)
+    report_list, duplicates = remove_duplicates(report_list)
+
+    # Handle duplicate reports
+    for dup in duplicates:
+        dup.set_name(f"Duplicate - {dup.name}")
+
+    # generator has to be able to cycle through the sequence
+    generator = cycle(("Annual Report", "Q1", "Q2", "Q3"))
+
+    # we generate a list of the expected dates then remove the day from each
+    expected_report_dates = list(truncate_day_generator(
+        expected_seqeunce(report_list[0].date_ended, report_list[-1].date_ended)))
+
+    # using the previously created variables we can fill in the gaps of the reports
+    report_list = fill_missing_gaps(
+        report_list, expected_report_dates)
+
+    # sort the files with the names
+    sort_files(report_list, generator)
--- pdf_reader.py
+++ pdf_reader.py
@@ -1,13 +1,15 @@
-from PyPDF2 import PdfReader
-from datetime import date
-from string import printable
 import os
+from datetime import date
+from string import printable, digits
 
-def find_document_end_date(document_path):
+from pypdf import PdfReader
 
-    MONTHS = ("January","February","March","April","May","June","July","August","September","October","November", "December")
+
+def document_text_processor(document_path):
+
     reader = PdfReader(document_path)
 
+    # extract first page of the generator function
     for index, page in enumerate(reader.pages):
         if index == 0:
             text = page.extract_text()
@@ -15,57 +17,148 @@ def find_document_end_date(document_path):
 
     # clean up
     text = text.title()
-    text = text.replace("  "," ")
+    text = text.replace("  ", " ")
     text = text.replace(",", " ")
     text = text.split()
 
-    error_list = [("Decembe","December")]
-
-    print(text)
-
+    # we copy text for the error loop as we cannot edit strings as we use them
     output_text = text.copy()[:200]
 
+    # I gave up so just add error to this list
+    # use form (error, correction)
+    error_list = [("Decembe", "December")]
     for error in error_list:
         if error[0] in text:
             output_text[text.index(error[0])] = error[1]
             output_text.pop(text.index(error[0]) + 1)
 
-    for index, word in enumerate(output_text):
+    return output_text
 
+
+def find_quater_report(text):
+
+    # All the forms begin with a dated period in format -> MONTH DAY YEAR
+    # By finding the index of the first mention of month we can find the rest of the date by offset values
+    # Loop becuase index can only find a single value at any time
+    MONTHS = ("January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December")
+
+    month_found = None
+    for index, word in enumerate(text):
         if word in MONTHS:
             month_found = index
 
-    for char in printable.replace("0123456789",""): # removes all characters that are not numbers
-        output_text[month_found+2] = output_text[month_found+2].lower().replace(char,"")
-    
-    day = int(output_text[month_found+1])
-    month = MONTHS.index(output_text[month_found]) + 1
-    year = int(output_text[month_found+2])
+    if not month_found:
+        return None
 
-    return date(int(year),month,day) # Returns a datetime object 
+    # while this alternative code does work it is bad practice to have 11 errors for each input
+    # for month in MONTHS:
+    #     try:
+    #         month_found = text.index(month)
+    #     except ValueError:
+    #         continue
+
+    # post processing
+
+    # Year date can contain other letters such as Or as a result of the previous processing
+
+    for char in printable.replace(digits, ""):
+        text[month_found+2] = text[month_found+2].lower().replace(char, "")
+
+    year = int(text[month_found+2])
+    month = MONTHS.index(text[month_found]) + 1
+    day = int(text[month_found+1])
+
+    # Returns a datetime object to manipulate easier
+    return date(int(year), month, day)
+
+
+def find_company_name(text):
+
+    company_name_location_start, company_name_location_end = 0, 0
+
+    for index, word in enumerate(text):
+        if "Number" in word:
+            company_name_location_start = index
+
+        if "Exact" in word:
+            company_name_location_end = index
+            break
+
+    if not (company_name_location_start and company_name_location_end):
+        return None
+
+    # array of company name
+    company_name = text[company_name_location_start +
+                        2: company_name_location_end]
+
+    # add a space between the words
+    output_name = [company_name[0]]
+
+    for index, word in enumerate(company_name):
+        if (index) % 2 == 0:
+            # print(word)
+            output_name.append(" ")
+        else:
+            output_name.append(word)
+
+    # remove any white space
+    output_name = "".join(output_name).strip()
+    return " ".join(output_name.split())
+
 
 class document:
-    def __init__(self,name, path=os.getcwd()):
-        self.name = name
+    def __init__(self, name, path=os.getcwd()):
 
-        # print(self.name)
+        self.name = name
         self.directory = path
-        
-        # Path location
-        
+
+        # File location
         for root, dirs, files in os.walk(path):
             if name in files:
                 self.path = os.path.join(root, name)
             else:
                 self.path = os.path.join(path, name)
 
-        self.date_ended = find_document_end_date(name)
+        document_text = document_text_processor(name)
 
-    def set_name(self,name):
+        # data
+        self.company = find_company_name(document_text)
+        self.report_type = document.find_report_type(document_text)
+        self.date_ended = find_quater_report(document_text)
+
+    def find_report_type(text):
+
+        report_type = ("10-K", "10-Q")
+        for word in text:
+            if word in report_type:
+                return word
+        return None
+
+    def set_name(self, name, log=False):
+
+        # Safety net
+        if name[:-4] != ".pdf":
+            if self.name[:-4] != ".pdf":
+                name += ".pdf"
+
         os.rename(self.path, os.path.join(self.directory, name))
+        self.name = name
+
+        if log:
+            print(f"{self.name} -> {name}")
+
+        return True
+
+    def move_file(self, destination, log=False):
+        os.rename(self.path, os.path.join(destination, self.name))
+        self.directory = destination
+
+        if log:
+            print(f"{self.path} -> {os.path.join(destination, self.name)}")
 
     def __repr__(self) -> str:
         return f"document(name={repr(self.name)}, date_ended={repr(self.date_ended)})"
-    
+
     def __str__(self) -> str:
         return self.name
